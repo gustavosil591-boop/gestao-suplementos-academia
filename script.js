11c69ae0963a30bdb4d9e7578464f239887eeed3
gustavosil591-boop <gustavosil591@gmail.com>
2026-05-19 02:23:54 +0000

    adicionando lote do produto, preço a venda e preço a custo

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ function carregarEstoque() {
             <td>${produto.nome}</td>
             <td>${produto.categoria}</td>
             <td>${produto.quantidade}</td>
+            <td>R$ ${produto.precoCusto}</td>
+            <td>R$ ${produto.precoVenda}</td>
+            <td>${produto.lote}</td>
             <td>${produto.validade}</td>
         `;
         tabelaCorpo.appendChild(linha);
@@ -25,6 +28,9 @@ form.addEventListener('submit', function(evento) {
         nome: document.getElementById('nome').value,
         categoria: document.getElementById('categoria').value,
         quantidade: document.getElementById('quantidade').value,
+        precoCusto: document.getElementById('precoCusto').value,
+        precoVenda: document.getElementById('precoVenda').value,
+        lote: document.getElementById('lote').value,
         validade: document.getElementById('validade').value
     };
 
